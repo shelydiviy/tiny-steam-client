a9586f7081a76927a8fd5e6e9e84f644de509a89
shelydiviy <telephon.inet@gmail.com>
2025-01-21 17:58:00 +0300

    Update build.sh
    r2

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 HL2SDK_DIR="/home/sprut/fake/hl2sdk"
 ASIO_DIR="/home/sprut/asio/asio"
-CRYPTOPP_DIR="/home/sprut/fake/cryptopp"
+CRYPTOPP_DIR="/home/sprut/fake/cryptopp-CRYPTOPP_8_7_0"
 
 cmake -DCMAKE_BUILD_TYPE=Release -DHL2SDK-CSGO:PATH=$HL2SDK_DIR -DASIO-SRC:PATH=$ASIO_DIR -DCRYPTOPP-SRC:PATH=$CRYPTOPP_DIR -DEXECUTABLE_OUTPUT_PATH=$PWD/build/Release
 make
